c1ab939ae61aaa547d68f68afe4162085a2ee272
Ingrid E. Rodrigues <88205254+ingrid-ev@users.noreply.github.com>
2022-02-14 22:12:58 -0300

    Revisão de C#

---
+++ AlgoritimosSequenciais/AlgoritimosSequenciais/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritimosSequenciais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor, valf, porcem;
+
+            Console.Write("Informe o valor de custo: ");
+            valor = double.Parse(Console.ReadLine());
+
+            Console.Write("Informe a porcentagem do lucro: ");
+            porcem = double.Parse(Console.ReadLine());
+
+            valf = valor + (valor * (100 / porcem));
+
+            Console.WriteLine("O valor do produto sera: "+ valf.ToString());
+            Console.ReadKey();
+        }
+    }
+}
